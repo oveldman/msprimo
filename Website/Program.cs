292ad7fc0a87cd5ea9390ed9f90f7da84c5eb586
Oscsar Veldman <oveldman@gmail.com>
2019-01-24 14:22:30 +0100

    Make HTTPS work

--- Website/Program.cs
+++ Website/Program.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Website
 {
@@ -19,6 +21,14 @@ namespace Website
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureKestrel((context, options) =>
+                {
+                    options.Listen(IPAddress.Loopback,  5000);
+                    options.Listen(IPAddress.Loopback, 5001, listenOptions => 
+                    { 
+                        listenOptions.UseHttps("localhost.pfx", "Random77PrimoLucky"); 
+                    });
+                })
                 .UseStartup<Startup>();
     }
 }
